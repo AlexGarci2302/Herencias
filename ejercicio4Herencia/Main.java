33ce8044cd91156711122ea995931db5377e9508
Alejandro <akexgarcia47@gmail.com>
2025-06-24 12:16:39 +0200

    Añado las demás clases

--- ejercicio4Herencia/Main.java
+++ ejercicio4Herencia/Main.java
@@ -3,7 +3,11 @@ package Herencia.ejercicio4Herencia;
 public class Main {
     public static void main(String[] args) {
         Figura a = new Circulo(7);
+        Figura b = new Rectangulo(10, 5);
+        Figura c = new Triangulo(10, 5);
 
         System.out.println("El área del circulo es: " + a.calcularArea());
+        System.out.println("El área del rectángulo es: " + b.calcularArea());
+        System.out.println("El área del rectángulo es: " + c.calcularArea());
     }
 }
